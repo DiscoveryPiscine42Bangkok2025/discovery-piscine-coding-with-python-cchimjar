f03a7af879f0c7eea6db2473f5680c2742b469bf
chchimja <67010196@kmitl.ac.th>
2025-09-17 12:49:28 +0700

    Add files via upload

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+else:
+    i = 0
+    while i <= 10:
+        j = 0
+        row = []
+        while j <= 10:
+            row.append(str(i * j))
+            j += 1
+        print(f"Table de {i}: {' '.join(row)}")
+        i += 1
